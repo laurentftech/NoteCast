24fc87481c5536b32b3b3179071b2c0e6418b627
Laurent FRANCOISE <lfrancoise@gmail.com>
2026-05-14 22:12:47 +0200

    fix: revert logging level to INFO for stable release

--- notecast/__main__.py
+++ notecast/__main__.py
@@ -22,7 +22,7 @@ from notecast.workers.poller_worker import PollerWorker
 from notecast.workers.transformer_worker import TransformerWorker
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
