d639f13ffb660b9894b4b634153557e649363fab
Laurent FRANCOISE <lfrancoise@gmail.com>
2026-05-14 20:35:11 +0200

    fix: temporarily set logging level to DEBUG for webhook debugging

--- notecast/__main__.py
+++ notecast/__main__.py
@@ -22,7 +22,7 @@ from notecast.workers.poller_worker import PollerWorker
 from notecast.workers.transformer_worker import TransformerWorker
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
